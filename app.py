27d67dd16b2be3417b52f8e02ce2ef044072bcdc
Iryna Holova <gis.golova@gmail.com>
2024-02-20 21:00:24 +0200

    add calculator cli

---
+++ app.py
@@ -0,0 +1,29 @@
+import argparse
+
+
+def add_numbers(a, b):
+    return a + b
+
+
+def subtract_numbers(a, b):
+    return a - b
+
+
+def print_result(action, a, b):
+    match action:
+        case 'add':
+            print(f'{a} + {b} = {add_numbers(a, b)}')
+        case 'subtract':
+            print(f'{a} - {b} = {subtract_numbers(a, b)}')
+        case _:
+            print(f'Unknown action "{action}"')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='Simple calculator', description='A CLI to add and subtract two numbers.')
+    parser.add_argument('action', type=str, help='One of: add or subtract')
+    parser.add_argument('a', type=float, help='First number')
+    parser.add_argument('b', type=float, help='Second number')
+    args = parser.parse_args()
+
+    print_result(args.action, args.a, args.b)
